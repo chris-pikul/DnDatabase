3f2544eb303e76c69c65157a201d28f5e88a9cbd
Chris Pikul <chris@workmaple.com>
2021-04-29 07:17:25 -0700

    starting work on the backgrounds added the interface, next up is the constructor and validators

--- ts/objects/background.ts
+++ ts/objects/background.ts
@@ -1,15 +1,133 @@
+import Reference, { NullReference, ReferenceItem, ReferenceLanguage, ReferenceSkill } from '../reference';
 import Resource, { IResource } from '../resource';
 import { ResourceType } from '../resource-type';
+import { OptionsArray } from '../options';
+import RollTable from '../roll-table';
 import TextSection from '../text-section';
 
+export interface ICharacterOptionals<ObjectType> {
+    /**
+     * The starting items that are provided as standard
+     * wares, not requiring and choosing.
+     */
+    starting : Array<ObjectType>;
+
+    /**
+     * An available list of options that can be chosen
+     * from. Each entry in this array is an option
+     * within themselves, so they should be "compounded"
+     * for each index.
+     */
+    options : OptionsArray<ObjectType>;
+}
+
+export interface IBackgroundProficienciesLanguages extends ICharacterOptionals<ReferenceLanguage> {
+    /**
+     * Provides the ability to choose any languages.
+     * The amount value tells how many to choose.
+     * The "standard" boolean declares wheather languages flagged as "standard" may be chosen.
+     * Same for "exotics".
+     */
+    wildcard : {
+        amount : Number;
+        standard : boolean;
+        exotic : boolean;
+    };
+}
+
+/**
+ * Declares the proficiencies that are provided (or selectable) by this background.
+ */
+export interface IBackgroundProficiencies {
+    skills      ?: ICharacterOptionals<ReferenceSkill>;
+    tools       ?: ICharacterOptionals<ReferenceItem>;
+    languages   ?: IBackgroundProficienciesLanguages;
+}
+
+/**
+ * Lists the equipment that is provided, and choosable
+ * for a background.
+ */
+export interface IEquipmentItems {
+    
+    starting : Array<ReferenceItem | Object>
+
+    
+    options : OptionsArray<ReferenceItem | Object>
+}
+
+/**
+ * The equipment "loadout" that is provided as part
+ * of the background.
+ */
+export interface IBackgroundEquipment {
+    /**
+     * The number of Gold Pieces (GP) that is provided.
+     */
+    coinPouch : Number
+
+    /**
+     * A reference to the clothing item that is provided.
+     */
+    clothes : ReferenceItem
+
+    /**
+     * The remaining items and optional item choices.
+     */
+    items : ICharacterOptionals<ReferenceItem | Object>
+}
+
+/**
+ * Enumeration for the "lifestyle" or budget
+ * that characters may have.
+ */
+ export enum Lifestyle {
+    WRETCHED        = "WRETCHED",
+    SQUALID         = "SQUALID",
+    POOR            = "POOR",
+    MODEST          = "MODEST",
+    COMFORTABLE     = "COMFORTABLE",
+    WEALTHY         = "WEALTHY",
+    ARISTOCRATIC    = "ARISTOCRATIC",
+}
+
 export interface IBackground extends IResource {
+    proficiencies   : IBackgroundProficiencies
+    equipment       : IBackgroundEquipment
+
+    lifestyle       ?: Lifestyle
+
     feature         ?: TextSection
+    
+    traits          ?: RollTable
+    ideals          ?: RollTable
+    bonds           ?: RollTable
+    flaws           ?: RollTable
 }
 
 export default class Background extends Resource implements IBackground {
     static readonly JSONSchemaFile = "background.schema.json";
 
     readonly type : ResourceType = ResourceType.BACKGROUND;
+
+    proficiencies : IBackgroundProficiencies = {};
+
+    equipment : IBackgroundEquipment = {
+        coinPouch: 40,
+        clothes: NullReference,
+        items: {
+            starting: [],
+            options: [],
+        },
+    };
+
+    lifestyle ?: Lifestyle;
+    feature ?: TextSection;
+
+    traits  ?: RollTable;
+    ideals  ?: RollTable;
+    bonds   ?: RollTable;
+    flaws   ?: RollTable;
 
     constructor(props:any) {
         super(ResourceType.BACKGROUND, props);
---
+++ ts/options.ts
@@ -0,0 +1,58 @@
+/**
+ * Provides an alotment of choices that may be
+ * chosen from.
+ * 
+ * The provided "amount" describes how many objects
+ * may be chosen from the choices provided.
+ * 
+ * Each "choices" entry is an array itself that must
+ * contain 1 or more objects. In most cases there is
+ * only 1. In the case of more than one per the entry,
+ * it is treated as an "OR" option.
+ * Example:
+ *      amount: 2,
+ *      choices: [
+ *          [ "sword" ],
+ *          [ "axe" ],
+ *          [ "longbow", "crossbow" ]
+ *      ],
+ * In this example, 2 options can be chosen. The items
+ * "sword", and "axe" can be chosen and the options are
+ * satisfied. Or "axe", and "crossbow" can be chosen.
+ * But! "longbow" and "crossbow" is not allowed, since
+ * the multi-entry array specifies an "OR" condition.
+ * 
+ */
+ export interface IOptions<Type> {
+    /**
+     * The amount of objects that may be chosen from
+     * the "choices" array.
+     */
+    amount   : Number;
+
+    /**
+     * The available choices, each index in the parent
+     * array being a single choice. It's child objects
+     * being treated as an "OR" statement within the
+     * sub-array.
+     * 
+     * @see IOptions definition for an example
+     */
+    choices ?: Array<Type>
+
+    /**
+     * NOTE: Remember what this was for.
+     * My guess is, I was planning on this being for
+     * usage with "categories" or generic items such
+     * as "melee weapon" meaning any within the category.
+     */
+    fromAny ?: boolean
+}
+
+/**
+ * Type alias for an array of options. Usually when used
+ * for equipment and character building there is a few
+ * choices that combine to build the "loadout".
+ * Ex. 2 weapons, 3 trinkets, 1 magic item, etc.
+ */
+export type OptionsArray<Type> = Array<IOptions<Type>>
