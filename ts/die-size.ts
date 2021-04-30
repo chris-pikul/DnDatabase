b243662f63a3d303c2d6176c7f4ad620f9216b96
Chris Pikul <chris@workmaple.com>
2021-04-29 22:27:30 -0700

    new enum for dice sizing

---
+++ ts/die-size.ts
@@ -0,0 +1,18 @@
+/**
+ * Holds enumerated values for die sizes.
+ * 
+ * The "UNKNOWN" enum maps to 0 for null values and should
+ * not be treated as valid.
+ */
+export enum DieSize {
+    UNKNOWN  = 0,
+
+    D2  = 2,
+    D4  = 4,
+    D6  = 6,
+    D8  = 8,
+    D10 = 10,
+    D12 = 12,
+    D20 = 20,
+    D100= 100,
+};
