a6253dd4b66b273318cc7e288d5d3dd74fdf5ce6
Chris Pikul <chris@workmaple.com>
2021-07-19 22:23:32 -0700

    completed action change over

--- ts/objects/action.ts
+++ ts/objects/action.ts
@@ -1,50 +1,213 @@
 import Resource, { IResource } from '../resource';
 import { ResourceType } from '../resource-type';
+import { ReferenceAction } from '../reference';
 import Source from '../source';
-import { CountedArray, CountedArrayElem } from '../utils/arrays';
+import { CountedArray, CountedArrayElem, StrictValidateCountedArray } from '../utils/arrays';
+import { IsPlainObject, JSONObject } from '../utils/json-object';
 
+/**
+ * Enumeration for the timing an action can take.
+ */
 export enum ActionTiming {
+    /**
+     * The timing is unknown and may be specified more clearly in whatever
+     * object, or spell, it's related to. Otherwise it may be up to the DM's
+     * discretion.
+     */
+    VARIES = 'VARIES',
+
     ACTION = 'ACTION',
     BONUS_ACTION = 'BONUS_ACTION',
     REACTION = 'REACTION',
-    VARIES = 'VARIES',
+
+    /**
+     * This action is free to do at any time and is not restricted by combat
+     * action economy.
+     */
     FREE = 'FREE',
 };
 
+/**
+ * An Action represents a choice a character performs, generally during the
+ * combat phases of play.
+ */
 export interface IAction extends IResource {
+    /**
+     * Is this from a "variant" rule source
+     */ 
     isVariant : boolean;
 
+    /**
+     * If this is a "variant" rule, where is it from
+     */
     variantSource ?: Source;
 
+    /**
+     * The timings this action uses. Each entry is considered an "OR" operation
+     * between them and is up to the DMs discretion.
+     * The number within each entry is how many of those "timings" are used.
+     * 
+     * For example the contents:
+     * [
+     *  { type: 'ACTION', count: 1 },
+     *  { type: 'REACTION', count: 1 },
+     * ]
+     * 
+     * Should be interpretted as the action taking "1 Action" OR "1 Reaction"
+     * but not both.
+     * 
+     * NOTE: The counted values are generally not necessary as everything will 
+     * most likely be 1, but is provided for expansion with homebrew.
+     */
     timing : CountedArray<ActionTiming>;
 };
 
+/**
+ * An Action represents a choice a character performs, generally during the
+ * combat phases of play.
+ */
 export default class Action extends Resource implements IAction {
+    /**
+     * The path to the JSON schmea file matching this type.
+     */
     static readonly JSONSchemaFile = "action.schema.json";
 
-    isVariant       : boolean;
-    variantSource  ?: Source;
-    timing          : CountedArray<ActionTiming>;
+    /**
+     * The base of the URI path to be used when generating URIs.
+     */
+     public static readonly URIBase = "/action";
 
-    constructor(props:any) {
-        super({
-            ...(props || {}),
+     /**
+      * Generates a new URI for this resource using the suggested URI-base of the
+      * class and the entities ID.
+      * 
+      * @param id string of the entity ID.
+      * @returns string URI
+      */
+     public static MakeURI = (id:string) => (`${Action.URIBase}/${id}`);
+ 
+     /**
+      * Holds the "Zero" value (empty, null) for easy reference
+      * and object instantiation.
+      */
+     public static readonly ZeroValue:Action = new Action();
+ 
+     /**
+      * Checks if the supplied object is at the class's zero value.
+      * @param obj Action object to check
+      * @returns True if the object has the default values
+      */
+     public static IsZeroValue = (obj:Action):boolean => (
+            obj.isVariant === false
+        &&  obj.variantSource === undefined
+        &&  obj.timing.length === 0
+     );
+ 
+     /**
+      * Generates a new Reference (sub-classed) that references the object by the
+      * given id and name.
+      * 
+      * Uses the class defaults for URI generation.
+      * 
+      * @param id string of the entity ID.
+      * @param name string of the entity Name.
+      * @returns Reference
+      */
+     public static MakeRef = (id:string, name:string):ReferenceAction => (
+         new ReferenceAction({
+             uri: Action.MakeURI(id),
+             name,
+         })
+     );
+ 
+     /**
+      * Performs type checking and throws errors if the
+      * properties needed are not the right types.
+      * Does not fully validate the data within them,
+      * but will check for emptyness, or incorrect Enums
+      * @throws TypeErrors for invalid properties
+      * @param props Incoming properties object
+      */
+     public static StrictValidateProps = (props:any):void => {
+        if(!props)
+            throw new TypeError(`Action.StrictValidateProps requires a valid parameter to check, none was given.`);
+        
+        if(props.isVariant) {
+            if(typeof props.isVariant !== 'boolean')
+                throw new TypeError(`Action "isVariant" property must be a boolean, instead found "${typeof props.isVariant}.`);
+        }
+
+        if(props.variantSource) {
+            if(typeof props.variantSource !== 'object')
+                throw new TypeError(`Action "variantSource" property must be an object, instead found "${typeof props.variantSource}.`);
+            Source.StrictValidateProps(props.variantSource);
+        }
+
+        if(!props.timing)
+            throw new TypeError(`Missing "timing" array for Action.`);
+        if(typeof props.timing !== 'object' || !Array.isArray(props.timing))
+            throw new TypeError(`Action "timing" property must be an array, instead found "${typeof props.timing}".`);
+        if(props.timing.length === 0)
+            throw new TypeError(`Action "timing" property cannot be empty.`);
+        StrictValidateCountedArray(props.timing, "string");
+     }
+
+    /**
+     * Is this from a "variant" rule source
+     */ 
+    isVariant : boolean;
+
+     /**
+      * If this is a "variant" rule, where is it from
+      */
+    variantSource ?: Source;
+ 
+     /**
+      * The timings this action uses. Each entry is considered an "OR" operation
+      * between them and is up to the DMs discretion.
+      * The number within each entry is how many of those "timings" are used.
+      * 
+      * For example the contents:
+      * [
+      *  { type: 'ACTION', count: 1 },
+      *  { type: 'REACTION', count: 1 },
+      * ]
+      * 
+      * Should be interpretted as the action taking "1 Action" OR "1 Reaction"
+      * but not both.
+      * 
+      * NOTE: The counted values are generally not necessary as everything will 
+      * most likely be 1, but is provided for expansion with homebrew.
+      */
+    timing : CountedArray<ActionTiming>;
+
+    constructor(props?:any) {
+        super(props, {
             type: ResourceType.ACTION,
+            uriBase: Action.URIBase,
         });
 
+        // Set defaults
         this.isVariant = false;
         this.timing = [];
 
-        //Only apply properties if there is a object parameter applied
-        if(props && typeof props === 'object') {
-            if(props.hasOwnProperty('isVariant') && typeof props.isVariant === 'boolean')
-                this.isVariant = props.isVariant;
-
-            if(props.hasOwnProperty('variantSource') && typeof props.variantSource === 'object' && props.variantSource.hasOwnProperty('publicationID'))
-                this.variantSource = props.variantSource;
-
-            if(props.hasOwnProperty('timing') && Array.isArray(props.timing))
-                this.timing = props.timing;
+        if(typeof props !== 'undefined' && props !== null) {
+            if(props instanceof Action) {
+                // This is another Action
+                // copy the properties in.
+                Action.StrictValidateProps(props);
+                this.isVariant = !!props.isVariant;
+                this.variantSource = new Source(props.variantSource);
+                this.timing = [ ...props.timing ];
+            } else if(IsPlainObject(props)) {
+                // This is a plain JSON object,
+                // attempt to assign the properties
+                Action.StrictValidateProps(props);
+                
+                this.assign(props);
+            } else {
+                console.warn(`Attempting to instantiate an AbilityScore object with an invalid parameter. Expected either an AbilityScore object, or a plain JSON object. Instead encountered a "${typeof props}".`);
+            }
         }
     }
 
